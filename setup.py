d78392f2202a28dc434c858e9f0ebf50350f2baf
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-07-29 21:42:26 -0500

    Pypi configuration

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'alakazam',
+    packages = ['alakazam'],
+    version = '0.1.0',
+    description = 'Functional programming sugar for Python',
+    author = 'Silvio Mayolo',
+    author_email = 'mercerenies@comcast.net',
+    license = 'BSD3',
+    url = 'https://github.com/Mercerenies/alakazam',
+    download_url = 'https://github.com/Mercerenies/alakazam/archive/0.1.tar.gz',
+    keywords = ['functional', 'sugar', 'syntax', 'lambda', 'stream', 'alakazam'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

138a8e3938e1974618c27c997432f43bafbfe00e
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-08-03 16:10:45 -0500

    Added indexing functions for consistency with getattr/setattr

--- alakazam/util.py
+++ alakazam/util.py
@@ -4,3 +4,14 @@ from functools import reduce
 def compose(*fs):
     """Composes a sequence of functions together."""
     return reduce(lambda g, h: lambda *a, **k: g(h(*a, **k)), fs)
+
+def setindex(object, index, value):
+    """Assigns a value to the specified index, using [] indexing."""
+    object[index] = value
+
+def getindex(object, index):
+    """Returns the value at the specified index. This is provided for
+    symmetry with setindex().
+
+    """
+    return object[index]

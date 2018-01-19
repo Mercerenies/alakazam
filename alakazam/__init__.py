761191171f514962845332ea67f7cec8c12aec30
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-01-19 11:55:41 -0600

    Tracing functions useful for debugging programs which run heavily in closures

--- alakazam/__init__.py
+++ alakazam/__init__.py
@@ -2,5 +2,6 @@
 from .lift import *
 from .util import *
 from .error import *
+from .trace import *
 from .anonymous import _1, _2, _3, _4, _5
 from .anonymous import *
---
+++ alakazam/trace.py
@@ -0,0 +1,14 @@
+
+import sys
+import traceback
+
+def trace(arg, res, **kwargs):
+    print(arg, **kwargs)
+    return res
+
+def traceid(res, **kwargs):
+    return trace(res, res, **kwargs)
+
+def tracestack(res, **kwargs):
+    traceback.print_stack(**kwargs)
+    return res

fd17839f99ab92a352af2c2f80bc5f2eeb8baba7
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-01-19 15:51:33 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 setup(
     name = 'alakazam',
     packages = ['alakazam'],
-    version = '0.3.0',
+    version = '0.4.0',
     description = 'Functional programming sugar for Python',
     long_description = read("README.rst"),
     author = 'Silvio Mayolo',

201156a6a3c8071a0bb4051d03ac782617ac9bdd
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-01-20 17:37:18 -0600

    "zero" object

--- alakazam/pattern.py
+++ alakazam/pattern.py
@@ -2,14 +2,22 @@
 from .error import *
 from .anonymous import *
 
+zero = object()
+
 class PatternClass(type):
 
     def __new__(cls, name, parents, dct):
         return type.__new__(cls, name, parents, dct)
 
     def __getitem__(self, args):
+
         def pattern(*iargs):
             return self.zz_apply(*iargs)
+
+        if not isinstance(args, tuple):
+            args = (args,)
+        if args == (zero,):
+            args = ()
         binder = bind(pattern)(*args)
         return binder
 
@@ -20,6 +28,3 @@ def pattern_class(cls):
 class Tup(object):
     def zz_apply(*args):
         return tuple(args)
-
-
-

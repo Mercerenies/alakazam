6fa9c70aef22eb035f7713963d7f143af0dca2b0
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-01-21 21:16:51 -0600

    Patterns and matching

--- alakazam/pattern.py
+++ alakazam/pattern.py
@@ -9,6 +9,34 @@ class etc(object):
     def __init__(self, name):
         self.name = name
 
+class PatternMatch(object):
+
+    def __init__(self):
+        self.okay = True
+        self.vars = {}
+        self.max  = 0
+
+    def __bool__(self):
+        return self.okay
+
+    def __nonzero__(self):
+        return self.__bool__()
+
+    def __setitem__(self, index, value):
+        if isinstance(index, int):
+            self.max = max(self.max, index + 1)
+        self.vars[index] = value
+
+    def __getitem__(self, index):
+        return self.vars.get(index, None)
+
+    def __delitem__(self, index):
+        if index in self.vars:
+            del self.vars[index]
+
+    def fail(self):
+        self.okay = False
+
 def _is_etc(obj):
     return obj is Ellipsis or isinstance(obj, etc)
 
@@ -17,6 +45,56 @@ def _etc_name(obj):
         return obj.name
     else:
         return None
+
+def _match_impl_seq(res, ptn, obj):
+    try:
+        curr = next(ptn)
+    except StopIteration:
+        # End of pattern; we should also be at the end of object sequence
+        try:
+            next(obj)
+        except StopIteration:
+            pass
+        else:
+            res.fail()
+        return
+    if _is_etc(curr):
+        # Matches the rest automatically
+        if _etc_name(curr) is not None:
+            _match_impl(res, _etc_name(curr), obj)
+        return
+    try:
+        test = next(obj)
+    except StopIteration:
+        # Out of objects but still have a pattern; oops
+        res.fail()
+        return
+    _match_impl(res, curr, test)
+    _match_impl_seq(res, ptn, obj)
+
+def _match_impl(res, ptn, obj):
+    if isinstance(ptn, Anon):
+        if not ptn._Anon__pattern:
+            raise AlakazamError("Non-pattern expression in pattern context")
+        new_ptn, test = ptn._Anon__pattern
+        if callable(test):
+            # It's a function; recurse
+            subobj = test(obj)
+            if subobj is None:
+                res.fail()
+            else:
+                _match_impl_seq(res, iter(new_ptn), iter(subobj))
+        else:
+            res[test] = obj
+    else:
+        # It's a literal; compare for equality
+        if ptn != obj:
+            res.fail()
+
+def match(ptn, obj):
+    res = PatternMatch()
+    _match_impl(res, ptn, obj)
+    return res
 
 class PatternClass(type):
 
@@ -35,7 +113,7 @@ class PatternClass(type):
         if args == (zero,):
             args = ()
         binder = bind(call)(*args)
-        binder._Anon__pattern = pattern
+        binder._Anon__pattern = (args, pattern)
         return binder
 
 def pattern_class(cls):
